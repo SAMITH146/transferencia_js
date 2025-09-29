22d14a79eb3e7d42f562e474f2f4fb6ded96a0e3
SAMITH <yimmersamithduarteplata624@gmail.com>
2025-09-29 17:36:16 -0500

    Se realiza el ejercicio1

---
+++ js/ejercicio1.js
@@ -0,0 +1,12 @@
+// Ejercicio 1: Calcular el total a pagar por un producto
+
+// Solicitar al usuario el nombre del producto, precio y cantidad
+let producto = prompt("Ingrese el nombre del producto:");
+let precio = prompt("Ingrese el precio:");
+let cantidad = prompt("Ingrese la cantidad:");
+
+// Calcular el total a pagar
+let total = precio * cantidad;
+// Mostrar el resultado
+alert("El total a pagar por " + cantidad + " " + producto + " es: " + total);
+console.log("El total a pagar por " + cantidad + " " + producto + " es: " + total);
