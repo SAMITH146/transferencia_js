4039b276f7cf3c2a48a03587b1cf5942cd6a1325
SAMITH <yimmersamithduarteplata624@gmail.com>
2025-09-29 17:46:05 -0500

    Se realiza el ejercicio3

---
+++ js/ejercicio3.js
@@ -0,0 +1,11 @@
+// Ejercicio 3: Pedir al usuario que ingrese las edades de tres personas y calcular el promedio de las edades.
+
+// Solicitar al usuario las edades
+let edad1 = prompt("Ingrese la edad de la primera persona:");
+let edad2 = prompt("Ingrese la edad de la segunda persona:");
+let edad3 = prompt("Ingrese la edad de la tercera persona:");
+// Calcular el promedio de las edades
+let promedio = (edad1) + (edad2) + (edad3) / 3;
+// Mostrar el resultado
+alert("El promedio de las edades es: " + promedio);
+console.log("El promedio de las edades es: " + promedio);
