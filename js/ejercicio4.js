c2ed1c0834dbbd83a9a80e5807adbcebed7ee666
SAMITH <yimmersamithduarteplata624@gmail.com>
2025-09-29 17:47:29 -0500

    Se realiza el ejercicio4

---
+++ js/ejercicio4.js
@@ -0,0 +1,10 @@
+// Ejercicio 4: Calcular el consumo de combustible de un vehículo
+
+// Solicitar al usuario los kilómetros recorridos y los litros consumidos
+let KmRecorridos = prompt("Ingrese los kilómetros recorridos:");
+let litrosConsumidos = prompt("Ingrese los litros consumidos:");
+// Calcular el consumo de combustible
+let consumo = KmRecorridos / litrosConsumidos;
+// Mostrar el resultado
+alert("El consumo de combustible es: " + consumo + " km/litro");
+console.log("El consumo de combustible es: " + consumo + " km/litro");
