8b98cde46993c40c328ec34f1baa6bdbb8996ee1
SAMITH <yimmersamithduarteplata624@gmail.com>
2025-09-29 17:41:46 -0500

    Se realiza el ejercicio2

---
+++ js/ejercicio2.js
@@ -0,0 +1,11 @@
+
+// Ejercicio 2: Calcular el salario semanal de un trabajador
+
+// Solicitar al usuario las horas trabajadas y el valor por hora
+let horas = prompt("Ingrese horas trabajadas:");
+let valorHora = prompt("Ingrese el valor por hora:");
+// Calcular el salario semanal
+let salario = horas * valorHora;
+// Mostrar el resultado
+alert("El salario semanal es: " + salario);
+console.log("El salario semanal es: " + salario);
