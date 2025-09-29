14b379579ff29d94183e079c83dad484c4a0a55b
SAMITH <yimmersamithduarteplata624@gmail.com>
2025-09-29 17:53:47 -0500

    Se realiza el ejercicio5

---
+++ js/ejercicio5.js
@@ -0,0 +1,10 @@
+// Ejercicio 5: Calcular el tiempo estimado de viaje
+
+// Solicitar al usuario la distancia y la velocidad promedio
+let distancia = prompt("Ingrese distancia (km):");
+let velocidad = prompt("Ingrese velocidad promedio (km/h):");
+// Calcular el tiempo estimado de viaje
+let tiempo = distancia / velocidad;
+// Mostrar el resultado
+alert("Tiempo estimado: " + tiempo + " horas");
+console.log("Tiempo estimado: " + tiempo + " horas");
