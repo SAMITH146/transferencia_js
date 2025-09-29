9d2b1f00fbecb037a5b1b7b50e5f2001a87ecf44
SAMITH <yimmersamithduarteplata624@gmail.com>
2025-09-29 17:54:57 -0500

    Se realiza el ejercicio7

---
+++ js/ejercicio7.js
@@ -0,0 +1,9 @@
+// Ejercicio 7: Calcular el precio final de un producto con descuento
+
+// Solicitar al usuario el precio del producto
+let precioProd = prompt("Ingrese precio del producto:");
+// Calcular el precio con descuento del 10%
+let final = precioProd * 0.9;
+// Mostrar el resultado
+console.log("Precio con 10% de descuento: " + final);
+alert("Precio con 10% de descuento: " + final);
